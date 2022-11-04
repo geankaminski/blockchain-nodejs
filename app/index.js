4c379d45092375022569e3a235da7cf095e6ba79
Gean Kaminski <geankaminski93@hotmail.com>
2022-11-04 13:51:10 -0300

    updating p2p

--- app/index.js
+++ app/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
+// npm run dev
+// HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001 npm run dev
+// HTTP_PORT=3003 P2P_PORT=5003 PEERS=ws://localhost:5001,ws://localhost:5002 npm run dev
+
 const app = express();
 const bc = new Blockchain();
+
+const p2pServer = new P2pServer(bc);
 
 app.use(express.json());
 
@@ -16,8 +23,12 @@ app.post('/mine', (req, res) => {
     const block = bc.addBlock(req.body.data);
     console.log(`New block added: ${block.toString()}`);
 
+    p2pServer.syncChains();
+
     res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+
+p2pServer.listen();
 
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -32,18 +32,23 @@ class P2pServer {
         console.log('Socket connected');
 
         this.messageHandler(socket);
-
-        socket.send(JSON.stringify(this.blockchain.chain));
+        this.sendChain(socket);
     }
 
     messageHandler(socket) {
         socket.on('message', message => {
             const data = JSON.parse(message);
-            console.log('data', data);
+            this.blockchain.replaceChain(data);
         });
+    }
+
+    syncChains() {
+        this.sockets.forEach(socket => this.sendChain(socket));
     }
 
     sendChain(socket) {
         socket.send(JSON.stringify(this.blockchain.chain));
     }
 }
+
+module.exports = P2pServer;
