bec7270040cc0c2262815bd6adbdbc1c93cebbb8
Gean Kaminski <geankaminski93@hotmail.com>
2022-11-07 19:44:46 -0300

    proof of work

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block.js');
+// const { DIFFICULTY } = require('../config');
 
 describe('Block', () => {
 
@@ -9,12 +10,24 @@ describe('Block', () => {
         lastBlock = Block.genesis();
         block = Block.mineBlock(lastBlock, data);
     })
-    
+
     it('sets the `data` to match the input', () => {
         expect(block.data).toEqual(data);
     })
 
     it('sets the `lastHash` to match the hash of the last Block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash);
+    })
+
+    it('generates a hash that matches the difficulty', () => {
+        expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+    })
+
+    it('lowers the difficulty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp + 360000)).toEqual(block.difficulty - 1);
+    })
+
+    it('raises the difficulty for quickly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp + 1)).toEqual(block.difficulty + 1);
     })
 });
